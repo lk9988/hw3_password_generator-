cfb2849431027c41e288264c698cf16a548ddc48
lana <lanabkim@gmail.com>
2020-11-05 23:44:15 -0800

    fixed alert and working now

--- script1.js
+++ script1.js
@@ -1,18 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
-
-// Add event listener to generate button
-// this is call back function - will only run then clicked
-generateBtn.addEventListener("click", writePassword);
 
 // Declare a list of special characters 
 
@@ -26,17 +16,17 @@ var specialSet = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-
 var numberSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
 
 // Declare  a list of lowercase characters 
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var lowerSet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 
 // Declare  a list of uppercase characters 
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var upperSet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 
 // Assignment Code / code generator button 
 var generateBtn = document.querySelector("#generate");
 
-var password; 
+ 
 
 
 
@@ -44,13 +34,15 @@ var password;
 
 function generatePassword() {
 
+
+
 //prompting the user for a passwordlength
 
   var passwordLength = prompt('How many characters would you like your password to contain? choose a number between 8 and 128 ! '); 
 
   
-// if not (passwordLength>=8 || passwordLength<=128) 
-if (!(passwordLength>=8 || passwordLength <=128)){
+// if not (passwordLength>=8 && passwordLength<=128) 
+if (!(passwordLength >= 8 && passwordLength <= 128)){
 
   // then Alert to the user that they need to provide a corrent length
   alert('invalid choice! please select a number between 8 ~ 128.');
@@ -74,9 +66,9 @@ var password = "";
 // IF 'isUsingNumbers'
 // Then push 'numbers'inton 'charactersToUSe' List 
 // and Append on random number from the 'numbers' list 
-var UseNumbers = confirm ('Use Numbers in your password?'); 
-if (UseNumbers == true){
-    charactersToUse = charactersToUse.push(numberSet); 
+var isUsingNumbers = confirm ('Use Numbers in your password?'); 
+if (isUsingNumbers == true){
+    charactersToUse = charactersToUse.concat(numberSet); 
 }
 
 
@@ -86,8 +78,12 @@ if (UseNumbers == true){
 // Then push 'specialSet' into charactersToUSe' List 
 // and Append on random number from the 'specialSet' list 
 
-var UseSpecial = confirm('Use Special Characters to you password'); 
+var isUsingSpecial = confirm('Use Special Characters to you password'); 
 
+if (isUsingSpecial == true){
+
+    charactersToUse = charactersToUse.concat(specialSet);     
+}
 
 
 
@@ -95,7 +91,11 @@ var UseSpecial = confirm('Use Special Characters to you password');
 // IF 'isUsingUpper'
 // Then push 'uppercase'into 'charactersToUSe' List 
 // and Append on random number from the 'numbers' list 
-
+ 
+var isUsingUpper = confirm ('Use UPPER-CASE in your password?'); 
+if (isUsingUpper == true){
+    charactersToUse = charactersToUse.concat(upperSet); 
+}
 
 
 
@@ -104,15 +104,24 @@ var UseSpecial = confirm('Use Special Characters to you password');
 // Then push 'lowercase '  into 'charactersToUSe' List 
 // and Append on random number from the 'numbers' list 
 
+var isUsingLower= confirm ('Use lower-case in your password?'); 
+if (isUsingLower == true){
+    charactersToUse = charactersToUse.concat(lowerSet); 
+}
 
-// If not 'charactersToUse.length'
-// then aleart to the user that they need to provide a correct length 
-//and exit function 
+ //check if user selects nothing 
 
 
 
-// for i = 0; continue as long as i < passwordLength; i++ 
-// for (var i = 0; i < passwordLength; i++) {
+if ( !isUsingNumbers && !isUsingSpecial && !isUsingUpper && !isUsingLower ) { 
+
+  alert('Please select options'); 
+  
+
+  return; 
+ }
+
+
 
 
 
@@ -122,11 +131,39 @@ var UseSpecial = confirm('Use Special Characters to you password');
 while (password.length < passwordLength) { 
 
 
-  // Select 'randonCharacter' a character from 'charactersToUse'
+  // Select 'randomCharacter' a character from 'charactersToUse'
+    var randomCharacter = charactersToUse[Math.floor(Math.random() * charactersToUse.length)]; 
+  
   // Append 'randomCharacter' to 'password' string 
 
+  password += randomCharacter; 
 
-// Return 'password' 
+
+
 }
+console.log(charactersToUse); 
+// Select Random characters from newlist characterstoUsefor 'password'
+
+// return'password'
 return password; 
 }
+
+
+
+// Write password to the #password input
+function writePassword() {
+
+  
+  
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+// this is call back function - will only run then clicked
+generateBtn.addEventListener("click", writePassword);
+
+
